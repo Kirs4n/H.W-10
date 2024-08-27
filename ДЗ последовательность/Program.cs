087657f6c4d7a9a4c28216502e41a8757df86655
KirKiller <KirKiller@DESKTOP-1JIA97R>
2024-08-27 22:23:15 +0500

    Добавьте файлы проекта.

---
+++ ДЗ последовательность/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ДЗ_последовательность
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayeYears = 365;
+            for (int i = 5; i <= dayeYears; i += 7)
+            {
+                Console.WriteLine($"День {i} c начала отчета");
+            }
+        }
+    }
+}
